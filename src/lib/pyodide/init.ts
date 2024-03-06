059ddd4e5dc0b2bc876907fc77dcbb42072933d8
Muspi Merol <wjj123345567@icloud.com>
2024-03-06 15:14:34 +0800

    chore: refactor pyodide `indexURL`

--- src/lib/pyodide/init.ts
+++ src/lib/pyodide/init.ts
@@ -1,12 +1,20 @@
 import type { ClientOptions } from "openai";
 
 import { pyodideReady } from "../stores";
+import { dev } from "$app/environment";
 import * as env from "$env/static/public";
 import { cacheSingleton } from "$lib/utils/cache";
 import { getEnv } from "$lib/utils/env";
 import { withToast } from "$lib/utils/toast";
 
-const indexURL = typeof window === "undefined" ? undefined : (process.env.NODE_ENV === "production" && "/pyodide/") || "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/";
+let indexURL: string | undefined;
+
+if (typeof window === "undefined")
+  indexURL = undefined;
+else if (dev)
+  indexURL = env.PUBLIC_PYODIDE_INDEX_URL ?? "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/";
+else
+  indexURL = env.PUBLIC_PYODIDE_INDEX_URL ?? "/pyodide/";
 
 async function initPyodide() {
   const { loadPyodide } = await import("pyodide");
