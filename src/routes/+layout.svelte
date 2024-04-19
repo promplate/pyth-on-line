362d371f8519e73a2a1693c8376ad86171f7b25f
Muspi Merol <wjj123345567@icloud.com>
2024-04-19 20:43:21 +0800

    tweaks: update configurations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,11 +47,11 @@
   }
 
   :global(body)::-webkit-scrollbar {
-    --uno: bg-neutral-9 w-1 sm: w-1.5 md: w-2 lg: w-2.5 xl: w-3 2xl: w-3.5;
+    --uno: bg-neutral-9 w-1 sm:w-1.5 md:w-2 lg:w-2.5 xl:w-3 2xl:w-3.5;
   }
 
   :global(body)::-webkit-scrollbar-thumb {
-    --uno: bg-neutral-7/30 hover: bg-neutral-7/70 rounded-l-sm;
+    --uno: bg-neutral-7/30 hover:bg-neutral-7/70 rounded-l-sm;
   }
 
   :global(body *)::-webkit-scrollbar {
