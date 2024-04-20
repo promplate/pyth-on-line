7aa47857692fa1b1a7fccb94798d13448ae51558
Muspi Merol <wjj123345567@icloud.com>
2024-04-20 23:49:01 +0800

    feat: support cross-line deletion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { AutoComplete, Item } from "$lib/components/console/HeadlessConsole.svelte";
+  import type { AutoComplete, Item, Status } from "$lib/components/console/HeadlessConsole.svelte";
   import type { PyProxy } from "pyodide/ffi";
   import type { ClipboardEventHandler, KeyboardEventHandler } from "svelte/elements";
 
@@ -23,6 +23,7 @@
 
   let pyConsole: PyProxy;
   let complete: AutoComplete;
+  let status: Status;
 
   let focusedError: { traceback: string; code: string };
 
@@ -141,6 +142,27 @@
         break;
       }
 
+      case "Backspace": {
+        if (inputRef.selectionStart === 0 && inputRef.selectionEnd === 0 && status === "incomplete") {
+          const item = log.at(-1)!;
+          const lines = item.text.split("\n");
+          pyConsole.buffer.pop();
+          if (lines.length === 1) {
+            log = [...log.slice(0, -1)];
+            input = lines[0] + input;
+            status = "complete";
+          }
+          else {
+            const lastLine = lines.pop()!;
+            log = [...log.slice(0, -1), { type: "in", text: lines.join("\n") }];
+            input = lastLine + input;
+          }
+          history.pop();
+          event.preventDefault();
+        }
+        break;
+      }
+
       default: {
         index = -1;
       }
@@ -172,7 +194,7 @@
 
 <div class="my-4 w-[calc(100vw-2rem)] flex flex-row gap-4 break-all p-3 text-neutral-3 <lg:(my-3 w-[calc(100vw-1.5rem)] gap-3 p-2 text-sm) <sm:(my-2 w-[calc(100vw-1rem)] gap-2 p-1 text-xs) [&>div]:(overflow-x-scroll rounded bg-white/3 p-5 <lg:p-4 <sm:p-3)">
   <div class="w-full flex flex-col gap-0.7 whitespace-pre-wrap font-mono [&>div:hover]:(rounded-sm bg-white/2 px-1.7 py-0.6 -mx-1.7 -my-0.6)">
-    <HeadlessConsole bind:ready bind:log bind:push bind:complete bind:pyConsole let:status let:loading>
+    <HeadlessConsole bind:ready bind:log bind:push bind:complete bind:pyConsole bind:status let:loading>
       {#each log as { type, text }, index}
         {#if type === "out"}
           <Out {text} />
