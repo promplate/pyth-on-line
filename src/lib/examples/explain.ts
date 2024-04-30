6c82adf6a9d8f6a31a91753d65fcb9be69fa4dcb
Muspi Merol <wjj123345567@icloud.com>
2024-04-30 18:50:37 +0800

    feat: support specifying `model` using environment variable

--- src/lib/examples/explain.ts
+++ src/lib/examples/explain.ts
@@ -1,10 +1,11 @@
 import getTemplates from "../../templates";
 import initPromplate from "../pyodide/entries";
+import * as env from "$env/static/public";
 
 export async function* explain(traceback: string, code: string) {
   const { AsyncChatGenerate } = await initPromplate();
 
   const context = await getTemplates();
 
-  yield * AsyncChatGenerate({})(await context.ExplainError.arender({ ...context, traceback, code }), { model: "gpt-3.5-turbo-0125", temperature: 0 });
+  yield * AsyncChatGenerate({})(await context.ExplainError.arender({ ...context, traceback, code }), { model: env.PUBLIC_LLM_MODEL ?? "gpt-3.5-turbo-0125", temperature: 0 });
 }
