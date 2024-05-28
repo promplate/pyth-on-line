b2aa1f53683ecb7bbccfafefe8cc0d7f6a8debe3
Muspi Merol <wjj123345567@icloud.com>
2024-05-28 15:35:05 +0800

    chore: update `pyodide` to version `0.26.0`

--- src/lib/pyodide/init.ts
+++ src/lib/pyodide/init.ts
@@ -13,7 +13,7 @@ let indexURL: string | undefined;
 if (typeof window === "undefined")
   indexURL = undefined;
 else if (dev)
-  indexURL = env.PUBLIC_PYODIDE_INDEX_URL ?? "https://cdn.jsdelivr.net/pyodide/v0.26.0a5/full/";
+  indexURL = env.PUBLIC_PYODIDE_INDEX_URL ?? "https://cdn.jsdelivr.net/pyodide/v0.26.0/full/";
 else
   indexURL = env.PUBLIC_PYODIDE_INDEX_URL ?? "/pyodide/";
 
