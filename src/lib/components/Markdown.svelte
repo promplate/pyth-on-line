02b75db1ac5f92dd5d1862d30f1d5ae0602b14f7
Muspi Merol <wjj123345567@icloud.com>
2024-05-31 10:28:43 +0800

    chore: fix style, downgrade `unocss` and update other deps

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -2,7 +2,7 @@
   export let html: string;
 </script>
 
-<article class="min-w-full text-sm prose [&>*:first-child]:mt-0 [&>*:last-child]:mb-0">
+<article class="min-w-full text-sm font-sans prose [&>*:first-child]:mt-0 [&>*:last-child]:mb-0">
   {@html html}
 </article>
 
