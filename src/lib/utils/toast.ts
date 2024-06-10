c6e2a883497a1ec5565f9fbb3aea05638d4e0c42
Muspi Merol <wjj123345567@icloud.com>
2024-06-10 20:28:08 +0800

    chore: fix typing

--- src/lib/utils/toast.ts
+++ src/lib/utils/toast.ts
@@ -1,8 +1,8 @@
 import { tick } from "svelte";
 import { toast } from "svelte-sonner";
 
-export function withToast<T extends (...args: any[]) => any>(data: { loading: string; success?: string; duration?: number }) {
-  return (anyFunction: T) => {
+export function withToast(data: { loading: string; success?: string; duration?: number }) {
+  return <T extends (...args: any[]) => any>(anyFunction: T) => {
     data.success = data.success ?? data.loading;
     return (async (...args) => {
       const promise: Promise<ReturnType<T>> = Promise.resolve(anyFunction(...args));
