0e7c8ecb1df457435e3ee63808b0842cc1085151
Muspi Merol <wjj123345567@icloud.com>
2024-06-23 17:47:48 +0800

    chore: remove unused code

--- src/lib/pyodide/translate.ts
+++ src/lib/pyodide/translate.ts
@@ -1,36 +1,3 @@
-import type { ClientOptions } from "openai";
-import type { PyProxy } from "pyodide/ffi";
-
-interface PyClientOptions {
-  api_key?: string;
-  base_url?: string;
-  organization?: string;
-  timeout?: number;
-  max_retries?: number;
-  defaultHeaders?: PyProxy;
-  defaultQuery?: PyProxy;
-}
-
-export function toJs(obj: PyProxy) {
-  return obj.toJs({ dict_converter: Object.fromEntries });
-}
-
-export async function toPyOptions(options: ClientOptions) {
-  const { getPy: initPy } = await import("./init");
-
-  const py = await initPy();
-
-  return {
-    api_key: options.apiKey,
-    base_url: options.baseURL,
-    organization: options.organization,
-    timeout: options.timeout,
-    max_retries: py.toPy(options.maxRetries),
-    defaultHeaders: py.toPy(options.defaultHeaders),
-    http_client: null,
-  } as PyClientOptions;
-}
-
 export function patchSource(source: string) {
   return (
     source.includes(">>> ")
