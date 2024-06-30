6e595f89bf7e966615ce10ef182a667739b4a14e
Muspi Merol <wjj123345567@icloud.com>
2024-07-01 03:52:01 +0800

    feat: simplify module unload process

--- src/lib/pyodide/load-sources.py
+++ src/lib/pyodide/load-sources.py
@@ -1,17 +1,13 @@
-from importlib import import_module, invalidate_caches
 from pathlib import Path
+from sys import modules
 from sys import path as sys_path
-from tempfile import TemporaryDirectory
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ...python import main
-
     sources: dict[str, str] = {}
 
 
-temp = TemporaryDirectory(prefix="runtime_")
-root = Path(temp.name)
+root = Path("/runtime_sources/python")
 for path, source in sources.items():
     file = root / path
     if not file.parent.is_dir():
@@ -19,11 +15,15 @@ for path, source in sources.items():
     file.write_text(source, "utf-8")
 
 sys_path.insert(0, root.parent.as_posix())
-globals().update(import_module(root.name).__dict__)
+for name in list(modules):
+    if name.startswith("python"):
+        del modules[name]
+        print(f"unloaded {name}")
+
+
+from python import main
 
 
 @main().add_done_callback
 def _(_):
-    temp.cleanup()
     sys_path.pop(0)
-    invalidate_caches()
