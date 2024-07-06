487d460f5125909aeb5cf16ef74412c4b17b57e9
Muspi Merol <wjj123345567@icloud.com>
2024-07-06 12:53:42 +0800

    chore: use a separate `rollup-plugin-flatten-dir` to import directories

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import dir2json from "./src/lib/plugins/dir2json";
 import { sveltekit } from "@sveltejs/kit/vite";
+import dir2json from "rollup-plugin-flatten-dir";
 import Unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [Unocss(), sveltekit(), dir2json({ includes: ["py", "j2"] })],
+  plugins: [Unocss(), sveltekit(), dir2json({ include: ["**/*.py", "**/*.j2"] })],
   assetsInclude: ["src/python/**/*"],
 });
