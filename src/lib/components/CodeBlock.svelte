65be6bf32b34f05c8b07786436ef505ea4ed491c
Muspi Merol <wjj123345567@icloud.com>
2024-07-21 18:55:09 +0800

    chore: remove unused code from `Code` component

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -1,26 +1,13 @@
 <script lang="ts">
   import { highlight } from "../highlight";
-  import Portal from "./Portal.svelte";
-  import { type SourceRef, saveSource } from "$lib/utils/source";
-  import { onMount } from "svelte";
 
   export let code: string;
-  export let collapse = false;
-  export let portal = false;
   export let lang = "text";
 
-  export let id: string | null = null;
-  export let previous: SourceRef[] = [];
-
   code = code.replaceAll("\r", "");
-
-  onMount(() => {
-    id && saveSource(id, code);
-  });
-
 </script>
 
-<section class:shrink-0={!collapse} class="not-prose relative overflow-y-scroll rounded-md [&>pre]:!line-height-relaxed">
+<section class="not-prose relative overflow-y-scroll rounded-md [&>pre]:!line-height-relaxed">
   {#key code}
     {#await highlight(lang, code)}
       <pre class="text-white/70">{code}</pre>
@@ -28,9 +15,6 @@
       {@html code}
     {/await}
   {/key}
-  {#if portal}
-    <Portal source={code} {previous} />
-  {/if}
 </section>
 
 <style>
