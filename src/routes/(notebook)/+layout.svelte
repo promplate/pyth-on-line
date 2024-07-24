e054b88c7267ab7b5397ae3ea1b21b4159f7d72f
Muspi Merol <wjj123345567@icloud.com>
2024-07-24 18:39:36 +0800

    fix(style): replace `max-w-` with `w-` to prevent shrinking

--- src/routes/(notebook)/+layout.svelte
+++ src/routes/(notebook)/+layout.svelte
@@ -2,7 +2,7 @@
   import { Button } from "bits-ui";
 </script>
 
-<div class="m-4 w-[calc(100%-2rem)] self-center lg:(m-7 max-w-2xl) md:m-6 sm:(m-5 max-w-xl w-auto) xl:m-8 2xl:max-w-4xl xl:max-w-3xl [&>article]:(lg:text-3.75 xl:text-base)">
+<div class="m-4 w-[calc(100%-2rem)] self-center 2xl:(m-10 w-4xl) lg:(m-7 w-2xl) md:m-6 sm:(m-5 w-xl) xl:(m-8 w-3xl) [&>article]:(lg:text-3.75 xl:text-base)">
 
   <nav class="w-full flex flex-row items-center justify-between gap-2 text-sm lg:text-base [&>a:hover]:op-80 [&>a]:(op-50 transition)">
     <Button.Root href="/">Home</Button.Root>
