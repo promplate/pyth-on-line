50382084885471946feff2bf55c0287d2da61c82
Muspi Merol <wjj123345567@icloud.com>
2024-07-26 23:25:02 +0800

    chore(style): correct and improve styling details

--- src/routes/playground/Tree.svelte
+++ src/routes/playground/Tree.svelte
@@ -16,13 +16,12 @@
     return parent ? `${parent}/${item.name}` : item.name;
   }
 
-  function countFlattenLength(children = tree) {
+  function countFlattenLength(children: Tree) {
     let length = 0;
     for (const item of children) {
+      length++;
       if (item.type === "folder" && !item.collapse)
         length += countFlattenLength(item.children);
-      else
-        length += 1;
     }
     return length;
   }
@@ -41,7 +40,7 @@
 
 <section>
   {#if parent !== ""}
-    <button style:--depth="{depth - 1 + 0.7}em" on:click={() => (folder.collapse = !collapse)}>
+    <button style:--depth="{depth - 1 + 0.8}em" on:click={() => (folder.collapse = !collapse)}>
       <div class={collapse ? "i-catppuccin-folder" : "i-catppuccin-folder-open"} />
       <div>{parent.split("/").at(-1)}</div>
     </button>
@@ -49,11 +48,11 @@
 
   {#if parent === "" || !collapse}
 
-    <section transition:slide={{ duration: 100 * (countFlattenLength() ** 0.5) }}>
+    <section transition:slide={{ duration: 100 * (countFlattenLength(tree) ** 0.5) }}>
 
       {#each tree as item}
         {#if item.type === "file"}
-          <button style:--depth="{depth + 0.7}em" class:!bg-neutral-8={focusedFile === getPath(item)} on:click={() => (focusedFile = getPath(item))}>
+          <button style:--depth="{depth + 0.8}em" class:!bg-neutral-8={focusedFile === getPath(item)} on:click={() => (focusedFile = getPath(item))}>
             <div class={getFileIcon(item)} />
             <div>{item.name}</div>
           </button>
@@ -74,5 +73,9 @@
 
   button {
     --uno: w-full flex shrink-0 flex-row items-center gap-1.4 overflow-x-hidden rounded-r-sm py-0.6 pl-$depth pr-1 text-left text-xs text-neutral-1/95 font-mono transition-background-color duration-100 active:bg-neutral-8/70 hover:bg-neutral-8/50;
+  }
+
+  div {
+    --uno: shrink-0;
   }
 </style>
