74f7a5b51d15f734c134965d0cdc2f144486674e
Muspi Merol <wjj123345567@icloud.com>
2024-07-27 23:40:38 +0800

    chore: extract component and rename things

--- src/routes/(workspace)/Workspace.svelte
+++ src/routes/(workspace)/Workspace.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-  import files from "../../python";
   import FileContent from "./FileContent.svelte";
   import FileList from "./FileList.svelte";
   import Console from "$lib/components/Console.svelte";
-  import SetupCodespace from "$lib/components/reusable/CodespaceLifecycle.svelte";
+  import SetupWorkspace from "$lib/components/reusable/WorkspaceLifecycle.svelte";
   import { Pane, PaneGroup, PaneResizer } from "paneforge";
 
-  let focusedFile: keyof typeof files = Object.keys(files)[0] as keyof typeof files;
+  export let sources: Record<string, string> = {};
+
+  let focusedFile: keyof typeof sources = Object.keys(sources)[0] as keyof typeof sources;
   let container: HTMLElement;
 </script>
 
 <div class="h-screen">
   <PaneGroup direction="horizontal">
     <Pane defaultSize={20} minSize={10}>
-      <FileList files={Object.keys(files).toSorted()} bind:focusedFile />
+      <FileList files={Object.keys(sources).toSorted()} bind:focusedFile />
     </Pane>
     <PaneResizer class="group">
       <div class="mx-1 h-full w-0.11em bg-white/10 transition group-active:bg-current group-hover:bg-white/30" />
@@ -21,7 +22,7 @@
     <Pane defaultSize={80} minSize={10}>
       <PaneGroup direction="vertical">
         <Pane defaultSize={70} minSize={10}>
-          <FileContent content={files[focusedFile]} lang={focusedFile.slice(focusedFile.lastIndexOf(".") + 1)} />
+          <FileContent content={sources[focusedFile]} lang={focusedFile.slice(focusedFile.lastIndexOf(".") + 1)} />
         </Pane>
         <PaneResizer class="group">
           <div class="my-1 h-0.11em w-full bg-white/10 transition group-active:bg-current group-hover:bg-white/30" />
@@ -29,7 +30,7 @@
         <Pane defaultSize={30} minSize={10}>
           <div class="h-full w-full overflow-y-scroll" bind:this={container}>
             <Console class="p-2 text-xs [&>div:hover]:rounded-r-none @2xl:(text-13px line-height-18px) @7xl:text-sm" {container} />
-            <SetupCodespace sources={files} />
+            <SetupWorkspace {sources} />
           </div>
         </Pane>
       </PaneGroup>
---
+++ src/routes/(workspace)/playground/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import files from "../../../python";
+  import Workspace from "../Workspace.svelte";
+</script>
+
+<Workspace sources={files} />
