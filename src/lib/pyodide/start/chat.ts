673361c092d698c6e0db2953b2924f3f1feb2107
Muspi Merol <wjj123345567@icloud.com>
2024-07-28 00:46:22 +0800

    chore: upgrade `promplate-pyodide` and simplify OpenAI patching

--- src/lib/pyodide/start/chat.ts
+++ src/lib/pyodide/start/chat.ts
@@ -1,25 +1,12 @@
-import type { ClientOptions } from "openai";
-
 import chat from "../../../python/chat";
 import { getPyodide, setupModule } from "./init";
-import * as env from "$env/static/public";
 import { cacheSingleton } from "$lib/utils/cache";
 import { OpenAI } from "openai";
 import * as version from "openai/version";
 
-class PatchedOpenAI extends OpenAI {
-  constructor(options: ClientOptions) {
-    if (!options.apiKey)
-      (options.apiKey = env.PUBLIC_OPENAI_API_KEY);
-    if (!options.baseURL)
-      options.baseURL = env.PUBLIC_OPENAI_BASE_URL;
-    super(options);
-  }
-}
-
 export const loadChat = cacheSingleton(async () => {
   const py = await getPyodide();
-  py.registerJsModule("openai", { OpenAI: PatchedOpenAI, version, __all__: [] });
+  py.registerJsModule("openai", { OpenAI, version, __all__: [] });
   await setupModule(chat, "chat");
   await py.pyimport("chat.install_requirements");
 });
