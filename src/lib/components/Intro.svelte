fd9bd102d2fe827eafc320a6873f7d6e56596490
Muspi Merol <wjj123345567@icloud.com>
2024-08-02 06:34:55 +0800

    feat: add an introduction page for GitHub repo workspace

---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+
+  export let title: string;
+</script>
+
+<div class="fixed inset-0 z--1 grid place-items-center">
+  <div class="flex flex-col gap-4 [&>a]:(w-fit flex flex-row items-center gap-2 b-2 b-neutral-8 rounded-lg px-3 py-2 text-sm transition) -translate-y-5 [&>a:hover]:bg-neutral-8">
+    <h1 class="mb-3 w-full gap-1 text-xl tracking-wide font-sans">
+      {#each title.split(" ") as char, i}
+        {#if i} <span> </span> {/if}
+        {@const t = Math.random() * (i + 1)}
+        <span class="inline-block" in:fly|global={{ delay: t * 50, duration: t * 40 + 300, y: Math.random() * 5 }}>{char}</span>
+      {/each}
+    </h1>
+    <slot />
+  </div>
+</div>
---
+++ src/routes/(workspace)/github/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { beforeNavigate } from "$app/navigation";
+  import Intro from "$lib/components/Intro.svelte";
+  import { toast } from "svelte-sonner";
+  import { fly } from "svelte/transition";
+
+  let input = "";
+  let ref: HTMLInputElement;
+
+  $: valid = input.split("/").length === 2 && !input.endsWith("/") && !input.startsWith("/");
+
+  beforeNavigate(({ complete, to }) => {
+    if (to?.route.id === "/(workspace)/github/[owner]/[repo]") {
+      const { owner, repo } = to.params!;
+      toast.promise(complete, { loading: `Fetching ${owner}/${repo}` });
+    }
+  });
+</script>
+
+{#await Promise.resolve() then}
+  <Intro title="Open a GitHub Repository">
+    <a href="github/promplate/pyth-on-line" in:fly|global={{ duration: 500, x: -5, delay: 100 }}>
+      <div class="i-tabler-inner-shadow-bottom-right text-lg text-rose" />
+      This site
+    </a>
+    <a href="github/promplate/core" in:fly|global={{ duration: 500, x: 5, delay: 150 }}>
+      <div class="i-tabler-building-lighthouse text-lg text-yellow" />
+      Our prompt engineering framework
+    </a>
+    <a href="github/pyodide/micropip" in:fly|global={{ duration: 500, x: -5, delay: 300 }}>
+      <div class="i-tabler-brand-python text-lg text-blue" />
+      Pyodide's Micropip
+    </a>
+    <a href="github/{input}" class="w-fit" class:cursor-text={!valid} on:click={e => !valid && ([e.preventDefault(), ref.focus()])} in:fly|global={{ duration: 500, x: 5, delay: 500 }}>
+      <div class="i-tabler-brand-github text-lg text-gray-4 transition-color" class:text-gray-1={valid} />
+      <input class="w-50 bg-transparent outline-none placeholder:(text-inherit op-30)" class:animate-pulse={!input} placeholder="Your GitHub repo" bind:this={ref} on:click|preventDefault={() => { ref.focus(); }} bind:value={input} on:keydown={e => e.key === "Enter" && ref.parentElement?.click()}>
+    </a>
+  </Intro>
+{/await}
