88656042cd5b9adc676639eff74972cf6cdda0db
Muspi Merol <wjj123345567@icloud.com>
2024-08-04 19:04:05 +0800

    feat: add auto `z-index` to `Modal` instances

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Modal from "$lib/components/Modal.svelte";
+  import { onMount } from "svelte";
+
+  let show = false;
+
+  onMount(() => {
+    setTimeout(() => {
+      show = true;
+    }, 100);
+  });
+</script>
+
+<Modal {show}>
+  <div slot="content" class="h-30 w-70 bg-cyan" />
+</Modal>
+
+<Modal show>
+  <div slot="content" class="h-60 w-60 bg-red">
+</Modal>
+
+<div class="fixed bottom-20 self-center">the cyan square should be on top of the red one</div>
