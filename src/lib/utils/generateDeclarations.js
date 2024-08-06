2d782269241d6eb2f59b65645aa5479c5de2f549
Muspi Merol <wjj123345567@icloud.com>
2024-08-06 08:08:51 +0800

    feat: add postinstall script to generate declarations

---
+++ src/lib/utils/generateDeclarations.js
@@ -0,0 +1,27 @@
+import { existsSync, readFileSync, readdirSync, statSync, writeFileSync } from "node:fs";
+import { join, resolve } from "node:path";
+
+const srcPath = "./src";
+
+const files = readdirSync(srcPath, { recursive: true, withFileTypes: true }).filter(file => file.isFile()).map(({ parentPath, name }) => `${parentPath}/${name}`);
+
+for (const file of files) {
+  if (!file.endsWith(".ts") && !file.endsWith(".svelte"))
+    continue;
+
+  const content = readFileSync(file, "utf-8");
+  const lines = content.split("\n");
+
+  for (const line of lines) {
+    const importMatch = line.match(/import\s.*from\s*["'](.+)["']/);
+    if (importMatch) {
+      const modulePath = resolve(file, "..", importMatch[1]);
+
+      if (existsSync(modulePath) && statSync(modulePath).isDirectory() && !existsSync(join(modulePath, "index.ts"))) {
+        // generate dts
+        writeFileSync(join(modulePath, "index.d.ts"), `export default {} as {${readdirSync(modulePath).map(name => `\n  ${JSON.stringify(name)}: string;`).join("")}\n}`, "utf-8");
+        console.warn(`Generated ${modulePath}/index.d.ts`);
+      }
+    }
+  }
+}
