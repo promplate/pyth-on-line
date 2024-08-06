d81ccbe3dd1e738ea69d270c14d9e0f5ce7271da
Muspi Merol <wjj123345567@icloud.com>
2024-08-07 05:18:08 +0800

    fix(style): prevent tooltips from unexpected reopening

--- src/lib/components/reusable/WithTooltip.svelte
+++ src/lib/components/reusable/WithTooltip.svelte
@@ -10,8 +10,8 @@
   <Tooltip.Trigger asChild let:builder>
     <slot {builder} />
   </Tooltip.Trigger>
-  <Tooltip.Content sideOffset={4}>
-    <div transition:scale={{ start: 0.95, duration: 300 }} class="rounded bg-neutral-2/10 px-0.5em py-0.3em text-xs text-neutral-3 backdrop-blur-sm">
+  <Tooltip.Content sideOffset={4} asChild let:builder>
+    <div transition:scale={{ start: 0.95, duration: 300 }} class="rounded bg-neutral-2/10 px-0.5em py-0.3em text-xs text-neutral-3 backdrop-blur-sm" {...builder} use:builder.action>
       {tips}
     </div>
   </Tooltip.Content>
