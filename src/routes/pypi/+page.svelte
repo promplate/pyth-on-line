07efa5554829deec44f33c83ad45bbf6a1f65bcf
Muspi Merol <wjj123345567@icloud.com>
2024-08-30 23:20:43 +0800

    feat: implement a basic view for search results

--- src/routes/pypi/+page.svelte
+++ src/routes/pypi/+page.svelte
@@ -4,7 +4,7 @@
   import { search } from "./store";
   import { browser } from "$app/environment";
   import { goto } from "$app/navigation";
-  import CodeBlock from "$lib/components/CodeBlock.svelte";
+  import { Button } from "bits-ui";
 
   export let data: PageServerData;
 
@@ -21,6 +21,21 @@
   }
 </script>
 
-<div class="text-sm [&_.line>span]:select-all [&>*>*]:p-5" class:op-50={loading}>
-  <CodeBlock lang="json" code={JSON.stringify(data, null, 4)} />
+<h1 class="text-xl">
+  <span class="text-neutral-1">{data.total}</span>
+  <span class="text-neutral-5">Results for</span>
+  <span class="text-neutral-1">{data.query}</span>
+</h1>
+
+<div class="my-4 flex flex-col gap-3 transition-opacity" class:op-50={loading}>
+  {#each data.results as { name, version, description = "", updated }}
+    <Button.Root class="flex flex-col gap-1 px-3 py-2 ring-(1.2 neutral-8) hover:ring-neutral-5" href="/pypi/{name}">
+      <div class="flex flex-row items-center gap-1.5 ws-nowrap">
+        <h2 class="text-lg text-neutral-1">{name}</h2>
+        <code class="rounded-sm bg-neutral-4/10 px-1 text-sm text-neutral-4">{version}</code>
+        <div class="w-full text-right text-neutral-4 text-neutral-7 <sm:text-sm">{updated}</div>
+      </div>
+      <h3 class="text-xs text-neutral-5 sm:text-sm">{description}</h3>
+    </Button.Root>
+  {/each}
 </div>
