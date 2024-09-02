3f5956c9d597a5f6643c49970b6a5460cadccbce
Muspi Merol <wjj123345567@icloud.com>
2024-09-02 19:27:46 +0800

    feat: translate `/github`

--- src/routes/(workspace)/github/+page.svelte
+++ src/routes/(workspace)/github/+page.svelte
@@ -18,18 +18,18 @@
 </script>
 
 {#await Promise.resolve() then}
-  <Intro title="Open a GitHub Repository">
+  <Intro title="打开一个 GitHub 仓库为工作区">
     <a href="github/promplate/pyth-on-line" in:fly|global={{ duration: 500, x: -5, delay: 100 }}>
       <div class="i-tabler-inner-shadow-bottom-right text-lg text-rose" />
-      This site
+      本项目
     </a>
     <a href="github/promplate/core" in:fly|global={{ duration: 500, x: 5, delay: 150 }}>
       <div class="i-tabler-building-lighthouse text-lg text-yellow" />
-      Our prompt engineering framework
+      我们的提示工程框架
     </a>
     <a href="github/pyodide/micropip" in:fly|global={{ duration: 500, x: -5, delay: 300 }}>
       <div class="i-tabler-brand-python text-lg text-blue" />
-      Pyodide's Micropip
+      pyodide/micropip
     </a>
     <a href="github/{input}" class="w-fit" class:cursor-text={!valid} on:click={e => !valid && ([e.preventDefault(), ref.focus()])} in:fly|global={{ duration: 500, x: 5, delay: 500 }}>
       <div class="i-tabler-brand-github text-lg text-gray-4 transition-color" class:text-gray-1={valid} />
