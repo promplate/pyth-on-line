f8cfd43f019f848c6d9a3b31a0c047b1141767b3
Muspi Merol <wjj123345567@icloud.com>
2024-09-02 21:36:55 +0800

    chore: rearrange the order of the links on `/`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,11 +11,11 @@
         <Hero />
       </div>
       <div class="flex flex-row flex-wrap gap-3 sm:(max-w-3xl self-center)">
-        <FeatureCard icon="i-solar-command-bold-duotone" on:click={() => $showMenu = true} title="试试快捷菜单" description="按 Ctrl/Cmd + K 打开" />
-        <FeatureCard icon="i-mdi-github-face" href="/github" title="打开一个 GitHub 仓库" description="从指定的 GitHub 仓库打开一个工作区" />
         <FeatureCard icon="i-ph-terminal-window-duotone" href="/console" title="Python 控制台" description="打开一个沉浸式控制台" />
         <FeatureCard icon="i-hugeicons-hand-pointing-right-01" href="/about" title="自我介绍" description="功能简介" />
+        <FeatureCard icon="i-solar-command-bold-duotone" on:click={() => $showMenu = true} title="试试快捷菜单" description="按 Ctrl/Cmd + K 打开" />
         <FeatureCard icon="i-hugeicons-package-search" href="/pypi" title="探索 PyPI" description="搜索PyPI上的包" />
+        <FeatureCard icon="i-mdi-github-face" href="/github" title="打开一个 GitHub 仓库" description="从指定的 GitHub 仓库打开一个工作区" />
         <FeatureCard icon="i-lucide-folder-dot" href="/playground" title="看看本站源码" description="本站的 Python 代码" />
       </div>
     </div>
