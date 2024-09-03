23b5ef31180aa414ccbec882dc8d5e7b2b16327c
Muspi Merol <wjj123345567@icloud.com>
2024-09-04 06:48:55 +0800

    chore: use absolute link to prevent potential bugs

--- src/routes/(workspace)/github/+page.svelte
+++ src/routes/(workspace)/github/+page.svelte
@@ -19,19 +19,19 @@
 
 {#await Promise.resolve() then}
   <Intro title="Open a GitHub Repository">
-    <a href="github/promplate/pyth-on-line" in:fly|global={{ duration: 500, x: -5, delay: 100 }}>
+    <a href="/github/promplate/pyth-on-line" in:fly|global={{ duration: 500, x: -5, delay: 100 }}>
       <div class="i-tabler-inner-shadow-bottom-right text-lg text-rose" />
       This site
     </a>
-    <a href="github/promplate/core" in:fly|global={{ duration: 500, x: 5, delay: 150 }}>
+    <a href="/github/promplate/core" in:fly|global={{ duration: 500, x: 5, delay: 150 }}>
       <div class="i-tabler-building-lighthouse text-lg text-yellow" />
       Our prompt engineering framework
     </a>
-    <a href="github/pyodide/micropip" in:fly|global={{ duration: 500, x: -5, delay: 300 }}>
+    <a href="/github/pyodide/micropip" in:fly|global={{ duration: 500, x: -5, delay: 300 }}>
       <div class="i-tabler-brand-python text-lg text-blue" />
       Pyodide's Micropip
     </a>
-    <a href="github/{input}" class="w-fit" class:cursor-text={!valid} on:click={e => !valid && ([e.preventDefault(), ref.focus()])} in:fly|global={{ duration: 500, x: 5, delay: 500 }}>
+    <a href="/github/{input}" class="w-fit" class:cursor-text={!valid} on:click={e => !valid && ([e.preventDefault(), ref.focus()])} in:fly|global={{ duration: 500, x: 5, delay: 500 }}>
       <div class="i-tabler-brand-github text-lg text-gray-4 transition-color" class:text-gray-1={valid} />
       <input class="w-50 bg-transparent outline-none placeholder:(text-inherit op-30)" class:animate-pulse={!input} placeholder="Your GitHub repo" bind:this={ref} on:click|preventDefault={() => { ref.focus(); }} bind:value={input} on:keydown={e => e.key === "Enter" && ref.parentElement?.click()}>
     </a>
