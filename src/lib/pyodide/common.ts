e11e3de933a629c82f3f6dbbcb803aecd856d858
Muspi Merol <wjj123345567@icloud.com>
2024-10-20 11:45:46 +0800

    feat: dynamically construct `indexURL`

--- src/lib/pyodide/common.ts
+++ src/lib/pyodide/common.ts
@@ -3,6 +3,6 @@ import { version } from "pyodide/package.json";
 
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-ignore
-export const indexURL = env.PUBLIC_PYODIDE_INDEX_URL ?? (import.meta.env.PROD ? `/pyodide/v${version}/` : `https://cdn.jsdelivr.net/pyodide/v${version}/full/`);
+export const indexURL = (env.PUBLIC_PYODIDE_INDEX_URL ?? (import.meta.env.PROD ? "/pyodide/v{}/" : "https://cdn.jsdelivr.net/pyodide/v{}/full/")).replace("{}", version);
 
 export const preloadPackages = ["micropip", "pure-eval"];
