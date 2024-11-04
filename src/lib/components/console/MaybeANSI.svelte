d6ba33dcca857156612c442d880202cbcf5b1b02
Muspi Merol <wjj123345567@icloud.com>
2024-11-04 16:24:15 +0800

    feat: support ANSI text highlighting

---
+++ src/lib/components/console/MaybeANSI.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Highlight from "./Highlight.svelte";
+
+  export let text: string;
+
+  // eslint-disable-next-line no-control-regex
+  const ansiPattern = /\x1B\[[0-9;]*[mK]/;
+</script>
+
+{#if ansiPattern.test(text)}
+  <Highlight text={text.replace(/\n$/, "")} lang="ansi" />
+{:else}
+  {text}
+{/if}
