9461be5ca6ba3d11435eb29c685a13a2e1ec9039
Muspi Merol <me@promplate.dev>
2025-02-19 20:03:32 +0800

    fix: update unocss config to use `presetWind3` instead of `presetUno`

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,10 @@
 import extractorSvelte from "@unocss/extractor-svelte";
-import { defineConfig, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from "unocss";
+import { defineConfig, presetIcons, presetTypography, presetWebFonts, presetWind3, transformerDirectives, transformerVariantGroup } from "unocss";
 
 const config = defineConfig({
   extractors: [extractorSvelte()],
   transformers: [transformerVariantGroup(), transformerDirectives()],
-  presets: [presetUno({ preflight: "on-demand" }), presetWebFonts({ provider: "none", fonts: { mono: "Fira Code Variable", sans: "Inter Variable" } }), presetIcons(), presetTypography()],
+  presets: [presetWind3({ preflight: "on-demand" }), presetWebFonts({ provider: "none", fonts: { mono: "Fira Code Variable", sans: "Inter Variable" } }), presetIcons(), presetTypography()],
 });
 
 export default config;
