edf07f45ccf0690fcbb8a087b5baa8e40a3856a2
Muspi Merol <me@promplate.dev>
2025-03-04 00:48:04 +0800

    chore: eslint --fix

--- src/lib/pyodide/api/explain.ts
+++ src/lib/pyodide/api/explain.ts
@@ -3,5 +3,5 @@ import getPy from "..";
 export async function* explain(traceback: string, code: string): AsyncGenerator<string> {
   const py = await getPy({ chat: true });
   const explain = py.pyimport("chat.explain.explain");
-  yield * explain(traceback, code);
+  yield* explain(traceback, code);
 }
