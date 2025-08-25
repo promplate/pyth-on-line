b7ff983df012a55cd6fcca7f4436b58468da3a30
Muspi Merol <me@promplate.dev>
2025-08-26 01:37:56 +0800

    chore: simplify `StepController`

--- tests/py/utils/time.py
+++ tests/py/utils/time.py
@@ -1,44 +1,22 @@
 from asyncio import Condition, Event
-from collections.abc import AsyncIterator
-from contextlib import asynccontextmanager
 
 
 class StepController:
     def __init__(self):
-        self._step_event = Event()
         self._step_condition = Condition()
-        self._current_step = 0
-        self._step_event.set()  # Initial state is triggered
-
-    @property
-    def current_step(self) -> int:
-        return self._current_step
+        self.current_step = 0
 
     async def step(self):
         """Advance one time step"""
         async with self._step_condition:
-            self._current_step += 1
-            self._step_event.set()
+            self.current_step += 1
             self._step_condition.notify_all()
 
     async def wait_for_step(self, step: int):
         """Wait until specified time step"""
         async with self._step_condition:
-            while self._current_step < step:
+            while self.current_step < step:
                 await self._step_condition.wait()
-            # Reset the event after waiting to prevent multiple triggers
-            self._step_event.clear()
-
-    async def wait_next_step(self):
-        """Wait for the next time step"""
-        current = self._current_step
-        await self.wait_for_step(current + 1)
-
-    @asynccontextmanager
-    async def at_step(self, step: int) -> AsyncIterator[None]:
-        """Context manager to execute at specified time step"""
-        await self.wait_for_step(step)
-        yield
 
 
 class TimeEvent:
