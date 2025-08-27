959aaae8dbab57775974b014c7a1c15626f7bd35
Muspi Merol <me@promplate.dev>
2025-08-28 02:26:37 +0800

    fix: refactor `__name_lookup` factory implementation in exec_hack

--- packages/hmr/reactivity/hmr/exec_hack/transform.py
+++ packages/hmr/reactivity/hmr/exec_hack/transform.py
@@ -6,7 +6,7 @@ class ClassTransformer(ast.NodeTransformer):
     @override
     def visit_ClassDef(self, node: ast.ClassDef):
         node.body = [
-            name_lookup_function,
+            *name_lookup_function,
             *map(ClassBodyTransformer().visit, node.body),
             ast.Delete(targets=[ast.Name(id="__name_lookup", ctx=ast.Del())]),
         ]
@@ -42,21 +42,21 @@ def build_name_lookup(name: str) -> ast.Call:
 
 name_lookup_source = """
 
-def __name_lookup(name):
+def __name_lookup():
     from builtins import KeyError, NameError
     from collections import ChainMap
     from inspect import currentframe
     f = currentframe().f_back
     c = ChainMap(f.f_locals, f.f_globals, f.f_builtins)
-    f = f.f_back
-    while f is not None and f.f_code.co_name != "<module>":
-        c.maps.insert(-2, f.f_locals)
-        f = f.f_back
-    try:
-        return c[name]
-    except KeyError as e:
-        raise NameError(*e.args) from None
+    def lookup(name):
+        try:
+            return c[name]
+        except KeyError as e:
+            raise NameError(*e.args) from None
+    return lookup
+
+__name_lookup = __name_lookup()
 
 """
 
-name_lookup_function = ast.parse(name_lookup_source).body[0]
+name_lookup_function = ast.parse(name_lookup_source).body
