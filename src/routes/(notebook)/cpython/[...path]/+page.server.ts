2a36103b2fbe20f1d93d192dd8605ad092fb5234
Muspi Merol <wjj123345567@icloud.com>
2025-09-07 20:44:14 +0800

    fix: update Python documentation base URL to `/3.13`

--- src/routes/(notebook)/cpython/[...path]/+page.server.ts
+++ src/routes/(notebook)/cpython/[...path]/+page.server.ts
@@ -3,6 +3,6 @@ import type { PageServerLoad } from "./$types";
 import { forwardFetch } from "$lib/utils/headers";
 
 export const load: PageServerLoad = async ({ params: { path }, request }) => {
-  const res = await forwardFetch(`https://docs.python.org/3.12/${path}`, request);
+  const res = await forwardFetch(`https://docs.python.org/3.13/${path}`, request);
   return { html: await res.text() };
 };
