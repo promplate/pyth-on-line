1b8b26128052d56d16dfa00803a48d311b9b0f98
Muspi Merol <me@promplate.dev>
2025-09-08 12:02:11 +0800

    feat(perf): don't load python in `/hmr/mcp`

--- src/routes/hmr/mcp/+page.svelte
+++ src/routes/hmr/mcp/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import NotebookLayout from "../../../routes/(notebook)/+layout.svelte";
   import mcpDocs from "./MCP.md?raw";
   import { page } from "$app/stores";
-  import Notebook from "$lib/components/notebook/Notebook.svelte";
+  import Router from "$lib/components/markdown/Router.svelte";
+  import WithMarkdown from "$lib/components/reusable/WithMarkdown.svelte";
 
   const url = `${$page.url.origin}/hmr/mcp`;
 
@@ -17,6 +17,8 @@
   const text = Object.entries(placeholders).reduce((acc, [key, value]) => acc.replaceAll(key, value), mcpDocs);
 </script>
 
-<NotebookLayout>
-  <Notebook {text} />
-</NotebookLayout>
+<div class="m-4 w-[calc(100%-2rem)] self-center 2xl:(m-10 w-4xl) lg:(m-7 w-2xl) md:m-6 sm:(m-5 w-xl) xl:(m-8 w-3xl) [&_article]:(lg:text-3.75 xl:text-base)">
+  <WithMarkdown let:parse>
+    <Router node={parse(text)} />
+  </WithMarkdown>
+</div>
