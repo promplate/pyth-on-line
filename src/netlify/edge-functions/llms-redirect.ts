072901a0b6a26d20fe10fd98de7381728f4bec39
Muspi Merol <wjj123345567@icloud.com>
2025-09-12 17:45:28 +0800

    fix: always return markdown content for llms on `llms.txt` route

--- src/netlify/edge-functions/llms-redirect.ts
+++ src/netlify/edge-functions/llms-redirect.ts
@@ -1,6 +1,18 @@
-import type { Config } from "@netlify/edge-functions";
+import type { Config, Context } from "@netlify/edge-functions";
 
-export default async () => Response.redirect("/hmr/llms", 302);
+import { isAIBot } from "ua-parser-js/helpers";
+
+export default async (request: Request, context: Context) => {
+  const userAgent = request.headers.get("user-agent") || "";
+  if (isAIBot(userAgent)) {
+    const res = await context.next();
+    res.headers.set("content-type", "text/markdown; charset=utf-8");
+    return res;
+  }
+  else {
+    return Response.redirect("/hmr/llms", 302);
+  }
+};
 
 export const config: Config = {
   path: "/hmr/llms*",
