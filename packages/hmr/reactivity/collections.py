dfe96fdd49aa377bf5a3f928ff42bed6597b10c9
Muspi Merol <wjj123345567@icloud.com>
2025-09-16 13:37:18 +0800

    feat: implement `ReactiveSequenceProxy`

--- packages/hmr/reactivity/collections.py
+++ packages/hmr/reactivity/collections.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-from collections.abc import Mapping, MutableMapping, MutableSet
+from collections.abc import Iterable, Mapping, MutableMapping, MutableSequence, MutableSet, Sequence
+from typing import overload
 
 from .context import Context, default_context
 from .primitives import Signal, Subscribable, _equal
@@ -7,7 +8,7 @@ from .primitives import Signal, Subscribable, _equal
 
 class ReactiveMappingProxy[K, V](MutableMapping[K, V]):
     def _signal(self, value=False):
-        return Signal(value, context=self.context)  # 0 for unset
+        return Signal(value, context=self.context)  # False for unset
 
     def __init__(self, initial: MutableMapping[K, V], check_equality=True, *, context: Context | None = None):
         self.context = context or default_context
@@ -61,7 +62,7 @@ class ReactiveMapping[K, V](ReactiveMappingProxy[K, V]):
 
 class ReactiveSetProxy[T](MutableSet[T]):
     def _signal(self, value=False):
-        return Signal(value, context=self.context)  # 0 for unset
+        return Signal(value, context=self.context)  # False for unset
 
     def __init__(self, initial: MutableSet[T], *, context: Context | None = None):
         self.context = context or default_context
@@ -110,6 +111,199 @@ class ReactiveSetProxy[T](MutableSet[T]):
 class ReactiveSet[T](ReactiveSetProxy[T]):
     def __init__(self, initial: set[T] | None = None, *, context: Context | None = None):
         super().__init__({*initial} if initial is not None else set(), context=context)
+
+
+class ReactiveSequenceProxy[T](MutableSequence[T]):
+    def _signal(self):
+        return Signal(context=self.context)
+
+    def __init__(self, initial: MutableSequence[T], check_equality=True, *, context: Context | None = None):
+        self.context = context or default_context
+        self._check_equality = check_equality
+        self._data = initial
+        self._keys = keys = defaultdict(self._signal)  # positive and negative index signals
+        self._iter = Subscribable()
+        self._length = len(initial)
+
+        for index in range(-len(initial), len(initial)):
+            keys[index] = self._signal()
+
+    @overload
+    def __getitem__(self, key: int) -> T: ...
+    @overload
+    def __getitem__(self, key: slice) -> list[T]: ...
+
+    def __getitem__(self, key: int | slice):
+        if isinstance(key, slice):
+            start, stop, step = key.indices(self._length)
+            if step != 1:
+                raise NotImplementedError  # TODO
+            for i in range(start, stop):
+                self._keys[i].get()
+            return self._data[start:stop]
+
+        else:
+            # Handle integer indices
+            self._keys[key].get()
+            if -self._length <= key < self._length:
+                return self._data[key]
+            raise IndexError(key)
+
+    def _replace(self, range_slice: slice, target: Iterable[T]):
+        start, stop, step = range_slice.indices(self._length)
+        if step != 1:
+            raise NotImplementedError  # TODO
+
+        target = [*target]
+        assert start <= stop
+        delta = len(target) - (stop - start)
+
+        with self.context.batch(force_flush=False):
+            if delta > 0:
+                if not self._check_equality:
+                    for i in range(start, self._length + delta):
+                        self._keys[i].notify()
+                    for i in range(stop + delta):
+                        self._keys[i - self._length - delta].notify()
+                else:
+                    for i in range(start, self._length + delta):
+                        if i < self._length:
+                            if i - start < len(target):
+                                if _equal(self._data[i], target[i - start]):
+                                    continue
+                            else:
+                                if _equal(self._data[i], self._data[i - delta]):
+                                    continue
+                        self._keys[i].notify()
+                    for i in range(stop + delta):
+                        if i >= delta:
+                            if i >= start:
+                                if _equal(self._data[i - self._length - delta], target[i - start]):
+                                    continue
+                            else:
+                                if _equal(self._data[i - self._length - delta], self._data[i - self._length]):
+                                    continue
+                        self._keys[i - self._length - delta].notify()
+
+            elif delta < 0:
+                if not self._check_equality:
+                    for i in range(start, self._length):
+                        self._keys[i].notify()
+                    for i in range(stop):
+                        self._keys[i - self._length].notify()
+                else:
+                    for i in range(start, self._length):
+                        if i < self._length + delta:
+                            if i - start < len(target):
+                                if _equal(self._data[i], target[i - start]):
+                                    continue
+                            else:
+                                if _equal(self._data[i], self._data[i - delta]):
+                                    continue
+                        self._keys[i].notify()
+                    for i in range(stop):
+                        if i >= -delta:
+                            if 0 <= i - start < len(target):
+                                if _equal(self._data[i - self._length], target[i - start]):
+                                    continue
+                            else:
+                                if _equal(self._data[i - self._length], self._data[i - self._length + delta]):
+                                    continue
+                        self._keys[i - self._length].notify()
+
+            else:
+                if not self._check_equality:
+                    for i in range(start, stop):
+                        self._data[i] = target[i - start]
+                        self._keys[i].notify()
+                        self._keys[i - self._length].notify()
+                else:
+                    for i in range(start, stop):
+                        original = self._data[i]
+                        if not _equal(original, target[i - start]):
+                            self._data[i] = target[i - start]
+                            self._keys[i].notify()
+                            self._keys[i - self._length].notify()
+
+            if delta:
+                self._length += delta
+                self._iter.notify()
+            self._data[start:stop] = target
+
+    def __len__(self):
+        self._iter.track()
+        return self._length
+
+    def __setitem__(self, key, value):
+        if isinstance(key, slice):
+            self._replace(key, value)
+        else:
+            if key < 0:
+                key += self._length
+            if not 0 <= key < self._length:
+                raise IndexError(key)
+            self._replace(slice(key, key + 1), [value])
+
+    def __delitem__(self, key):
+        if isinstance(key, slice):
+            self._replace(key, [])
+        else:
+            if key < 0:
+                key += self._length
+            if not 0 <= key < self._length:
+                raise IndexError(key)
+            self._replace(slice(key, key + 1), [])
+
+    def insert(self, index, value):
+        if index < 0:
+            index += self._length
+        if index < 0:
+            index = 0
+        if index > self._length:
+            index = self._length
+        self._replace(slice(index, index), [value])
+
+    def append(self, value):
+        self._replace(slice(self._length, self._length), [value])
+
+    def extend(self, values):
+        self._replace(slice(self._length, self._length), values)
+
+    def pop(self, index=-1):
+        if index < 0:
+            index += self._length
+        if not 0 <= index < self._length:
+            raise IndexError(index)
+        value = self._data[index]
+        self._replace(slice(index, index + 1), [])
+        return value
+
+    def remove(self, value):
+        for i in range(self._length):
+            if self._data[i] == value:
+                self._replace(slice(i, i + 1), [])
+                return
+        raise ValueError(value)
+
+    def clear(self):
+        self._replace(slice(0, self._length), [])
+
+    def reverse(self):
+        self._replace(slice(0, self._length), reversed(self._data))
+
+    def sort(self, *, key=None, reverse=False):
+        self._replace(slice(0, self._length), sorted(self._data, key=key, reverse=reverse))  # type: ignore
+
+    def __repr__(self):
+        return repr([*self])
+
+    def __eq__(self, value):
+        return [*self] == value
+
+
+class ReactiveSequence[T](ReactiveSequenceProxy[T]):
+    def __init__(self, initial: Sequence[T] | None = None, check_equality=True, *, context: Context | None = None):
+        super().__init__([*initial] if initial is not None else [], check_equality, context=context)
 
 
 # TODO: use WeakKeyDictionary to avoid memory leaks
--- tests/py/test_collections.py
+++ tests/py/test_collections.py
@@ -1,5 +1,6 @@
+from pytest import raises
 from reactivity import create_effect
-from reactivity.collections import ReactiveMappingProxy, ReactiveSetProxy
+from reactivity.collections import ReactiveMappingProxy, ReactiveSequenceProxy, ReactiveSetProxy
 from utils import capture_stdout
 
 
@@ -52,3 +53,63 @@ def test_reactive_set_proxy():
 
 def test_reactive_mapping_repr():
     assert repr(ReactiveMappingProxy({"a": 1})) == "{'a': 1}"
+
+
+def test_reactive_sequence_length():
+    seq = ReactiveSequenceProxy([1, 2, 3])
+    with capture_stdout() as stdout, create_effect(lambda: print(len(seq))):
+        assert stdout.delta == "3\n"
+        del seq[:]
+        assert stdout.delta == "0\n"
+        seq.extend([1, 2, 3, 4])
+        assert stdout.delta == "4\n"
+        seq.pop()
+        seq.pop()
+        assert stdout.delta == "3\n2\n"
+        seq.reverse()
+        assert stdout.delta == ""
+        assert seq == [2, 1]
+        seq[0:0] = [3, 4]
+        assert stdout.delta == "4\n"
+        seq.remove(3)
+        assert stdout.delta == "3\n"
+
+
+def test_reactive_sequence_setitem():
+    seq = ReactiveSequenceProxy([0, 0], check_equality=True)
+    with capture_stdout() as stdout, create_effect(lambda: print(seq[1])):
+        assert stdout.delta == "0\n"
+        seq.insert(0, 1)
+        assert stdout.delta == ""
+        seq.insert(0, 1)
+        assert stdout.delta == "1\n"
+        seq[1] = 2
+        assert stdout.delta == "2\n"
+        with raises(IndexError):
+            seq.clear()
+
+
+def test_reactive_sequence_negative_index():
+    seq = ReactiveSequenceProxy([0])
+    with capture_stdout() as stdout, create_effect(lambda: print(seq[-1])):
+        assert stdout.delta == "0\n"
+        seq.append(1)
+        assert stdout.delta == "1\n"
+        seq.extend([0, 1])
+        assert stdout.delta == ""
+        seq.pop()
+        assert stdout.delta == "0\n"
+        seq[-1] = 20
+        assert stdout.delta == "20\n"
+        seq.insert(0, 10)
+        assert stdout.delta == ""
+
+
+def test_reactive_sequence_slice_operations():
+    seq = ReactiveSequenceProxy([1, 2, 3, 4])
+    with capture_stdout() as stdout, create_effect(lambda: print(seq[1:2])):
+        assert stdout.delta == "[2]\n"
+        seq[-3:-1] = [20, 30]
+        assert stdout.delta == "[20]\n"
+        seq[-3] = 200
+        assert stdout.delta == "[200]\n"
