a2affdf4bef0b18a7fa8146f8ff9fa5b169d9441
Muspi Merol <wjj123345567@icloud.com>
2025-09-20 05:40:24 +0800

    chore: move the notebook introduction into the route folder

--- src/routes/(notebook)/about/+page.svelte
+++ src/routes/(notebook)/about/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import readme from "../../../../README.md?raw";
+  import readme from "./intro.md?raw";
   import Notebook from "$lib/components/notebook/Notebook.svelte";
 </script>
 
