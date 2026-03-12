e2a8f1b04523617dc123ac4eaa86e3383677a628
Muspi Merol <wjj123345567@icloud.com>
2026-03-13 02:05:07 +0800

    feat: set SvelteKit prerender origin from Vercel or Netlify env

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const vercelOrigin = process.env.VERCEL && `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+const netlifyOrigin = process.env.NETLIFY && process.env.DEPLOY_PRIME_URL;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter(),
+    prerender: {
+      origin: process.env.SITE_URL ?? vercelOrigin ?? netlifyOrigin,
+    },
     alias: {
       "$py/*": "src/python/*",
     },
